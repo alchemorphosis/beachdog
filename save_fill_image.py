ac544b431ca89ae0f06aa343a19caaa900c30a7d
alchemorhosis <robert@rlogan.com>
2024-11-29 13:51:17 -0600

    update to add back in letter printing for saveFillImage()

--- save_fill_image.py
+++ save_fill_image.py
@@ -11,9 +11,6 @@ def userInputSaveAsFile(title:str='Select a file to save'):
         )
     return filename
 
-
-
-
 def saveFillImage(puzzleGrid, blockSize, lineWidth):
 
     rows = len(puzzleGrid)
@@ -35,19 +32,7 @@ def saveFillImage(puzzleGrid, blockSize, lineWidth):
                 x1, y1 = j * blockSize + lineWidth, i * blockSize + lineWidth
                 x2, y2 = x1 + blockSize, y1 + blockSize
                 draw.rectangle(((x1, y1), (x2, y2)), fill='#000000', width=0)
-    # Fill in the letters
-    # for i in range(rows):
-    #     for j in range(cols):
-    #         x = int(j * blockSize + (lineWidth) + blockSize / 2)
-    #         y = int(i * blockSize + (lineWidth + lineWidth) + blockSize / 2)
-    #         draw.text(
-    #             (x, y),
-    #             text=puzzleGrid[i][j],
-    #             fill='#000000',
-    #             anchor='mm',
-    #             font=ImageFont.truetype('assets\\Monaco.ttf', int(blockSize / 2 + 3))
-    #         )
-    # Draw grid lines
+    #   Draw grid lines
     for i in range(rows + 1):
         y = i * blockSize# + lineWidth
         draw.line(((0, y), (canvasWidth - lineWidth, y)), fill='#000000', width=lineWidth)
